9cbd75fcee2669340fde10e28c7581569f7fc203
재욱 <haensol976520@gmail.com>
2025-11-05 11:11:09 +0900

    refector: 기존 파일 제거

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { VirtualList } from "./components/VirtualList";
-export { InfiniteList } from "./components/InfiniteList";
-export type { VirtualListProps, InfiniteListProps, PageResponse, Range } from "./types";
-
