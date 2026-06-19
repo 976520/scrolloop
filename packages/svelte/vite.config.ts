2362d52be676afc8dece696ea60165067edc8d25
재욱 <haensol976520@gmail.com>
2026-06-20 02:32:23 +0900

    fix(vite.config): update external dependencies to use regex for Svelte

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -11,12 +11,7 @@ export default defineConfig({
       fileName: (format) => `index.${format === "es" ? "mjs" : "cjs"}`,
     },
     rollupOptions: {
-      external: [
-        "svelte",
-        "svelte/store",
-        "@scrolloop/core",
-        "@scrolloop/shared",
-      ],
+      external: [/^svelte($|\/)/, "@scrolloop/core", "@scrolloop/shared"],
     },
   },
 });
